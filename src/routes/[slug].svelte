31801eeebaa12a4767d6fe95ba04ccee2f419ef6
Leonardo Matos <leomp120894@gmail.com>
2022-02-16 18:55:14 -0300

    Setup svelte kit and some @ecomplus libs
    Start product description ssr and hydration at /kit-snack-caramelizado

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  import ecomUtils from '@ecomplus/utils';
+  import ecomClient from '@ecomplus/client';
+
+  let description = 'foo';
+
+  export async function load({ params }) {
+    switch (params.slug) {
+      case 'kit-snack-caramelizado':
+        ecomUtils.$ecomConfig.set('store_id', 1024);
+        const { data } = await ecomClient.store({
+          url: '/products/5d09197e24607a6a42d7140d',
+        });
+        description = data.body_html;
+        break;
+    }
+    if (params.bar === 'def') {
+      return { fallthrough: true };
+    }
+    return {};
+  }
+</script>
+
+<p>{@html description}</p>
