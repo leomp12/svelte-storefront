d0dd5d8f2c8e7f2e3d1ef3d297c590f9e2604e27
Leonardo Matos <leomp120894@gmail.com>
2022-02-28 15:37:48 -0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script context="module">
+  let rand;
+  export async function load() {
+    rand = parseInt(Math.random() * 1000, 10);
+    return { maxage: 300 };
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>rand: {rand}</p>
